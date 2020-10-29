6a378cf9ec7530708069fd3bfc0a08b8d019dbcf
Thomas Graft <33382058+ThomasGraft@users.noreply.github.com>
2020-10-29 17:07:12 -0400

    Added administration capabilities to view orders and change their status, and CRUD operations for products.

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
 using SportsStore.Models;
 
 namespace SportsStore.Controllers
@@ -19,6 +22,8 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult Checkout() => View(new Order());
+
+        
 
         [HttpPost]
         public IActionResult Checkout(Order order)
